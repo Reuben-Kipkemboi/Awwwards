6eb8613fc9d8e6c007f1e6cad585568a8334bb94
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-06-10 14:15:55 +0300

    ft:application-registration - template initilaization and app registration

--- awards/settings.py
+++ awards/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app', 
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,13 @@ WSGI_APPLICATION = 'awards.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dbawards',
+        'USER': 'moringa',
+        'PASSWORD': 'Access',
+        'HOST': '',
+       
+        
     }
 }
 
