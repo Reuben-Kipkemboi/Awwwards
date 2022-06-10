ed20f1f99bd0686c250fa1401488a66f68ebf117
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-06-10 19:45:13 +0300

    ft:navbar - optimize navbar styling

--- app/urls.py
+++ app/urls.py
@@ -4,10 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     
-    path('', views.user_login, name='login'),
+    path('login/', views.user_login, name='login'),
     
-    path('', views.register, name='register'),
+    path('register/', views.register, name='register'),
     
-    path('', views.user_logout, name='logout'),
+    path('logout/', views.user_logout, name='logout'),
     
 ]
