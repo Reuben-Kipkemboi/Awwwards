494b141a6f52118ce8f636950dff927abce05a4e
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-06-11 08:17:12 +0300

    pt:user-login - fix user login csrf_token error

--- app/views.py
+++ app/views.py
@@ -33,7 +33,15 @@ def register(request):
 
 #user login function
 def user_login(request):
-    
+    if request.method =='POST':
+        username = request.POST['username']
+        password = request.POST['password']  
+        
+        user = authenticate (request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,"Welcome , you are now logged in")
+            return redirect ("home")
     return render(request, 'login.html')
 
 
