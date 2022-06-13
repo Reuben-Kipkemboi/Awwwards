bc284d3f184bd0cff8170e8678b7724e1c6a9d3d
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-06-13 20:22:16 +0300

    ts:user-test- updates user tests

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Project, Profile, Rating
 
-# Create your tests here.
+class TestProfile(TestCase):
+    def setUp(self):
+        self.new_user=User(first_name='Reuben', last_name='Kipkemboi', username='Reuby', email='reuby@gmail.com',password='password')
+        self.new_user.save()
+
+        self.new_profile=Profile(
+            user=self.new_user,profile_pic='cool_pic.jpg',username = 'Reuby',bio = 'African in New york',contacts = '0723987838 Nairobi'
+        )
